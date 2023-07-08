c8079fc208c96d59fb7c66b173c07a9f2df0e309
PomereningOskar <oskar.staskiewicz1@wp.pl>
2023-07-08 21:48:23 +0200

    suma zmiennych

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+let firstNumber = 24
+let secondNumber = 5
+
+function add(firstNumber, secondNumber) {
+    let result = firstNumber + secondNumber;
+    return result
+}
+
+console.log(add(firstNumber, secondNumber) );
